661b31bf0ef2314d1fd946ed0cc548cb2715ebe0
Yohan Lasorsa <noda@free.fr>
2021-04-27 08:46:29 +0000

    Configure GitOps with NubesGen

---
+++ terraform/modules/app-service/main.tf
@@ -0,0 +1,47 @@
+
+# This creates the plan that the service use
+resource "azurerm_app_service_plan" "application" {
+  name                = "plan-${var.application_name}-001"
+  resource_group_name = var.resource_group
+  location            = var.location
+
+  kind     = "Linux"
+  reserved = true
+
+  tags = {
+    "environment" = var.environment
+  }
+
+  sku {
+    tier = "Standard"
+    size = "S1"
+  }
+}
+
+# This creates the service definition
+resource "azurerm_app_service" "application" {
+  name                = "app-${var.application_name}-001"
+  resource_group_name = var.resource_group
+  location            = var.location
+  app_service_plan_id = azurerm_app_service_plan.application.id
+  https_only          = true
+
+  tags = {
+    "environment" = var.environment
+  }
+
+  site_config {
+    linux_fx_version = "NODE|14-lts"
+    app_command_line = "npm run start:prod"
+    always_on        = true
+    ftps_state       = "FtpsOnly"
+  }
+
+  app_settings = {
+    "WEBSITES_ENABLE_APP_SERVICE_STORAGE" = "false"
+    "WEBSITE_RUN_FROM_PACKAGE" = "1"
+    "WEBSITE_NODE_DEFAULT_VERSION" = "~14"
+
+    # These are app specific environment variables
+  }
+}
